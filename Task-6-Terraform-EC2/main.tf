b5c95619803ab049474938b28462fe952c607a7b
varsha <152709598+Varsha-yadav31@users.noreply.github.com>
2025-12-16 19:35:18 +0530

    Format main.tf with proper code structure

--- Task-6-Terraform-EC2/main.tf
+++ Task-6-Terraform-EC2/main.tf
@@ -13,3 +13,20 @@ Instance type
 Key pair
 
 Security group
+
+
+code:
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "Terraform-EC2-Instance"
+  }
+}
